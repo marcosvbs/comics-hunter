2bf526667735790c39394286bf21a0abe06d1a37
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2023-10-05 09:23:15 -0300

    add date formatter and data handle in `Card.tsx`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,39 @@ import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { Card } from "./components/Card";
 
-import coverImg from "./assets/comics-cover.png";
+import comic1Cover from "./assets/comic1Cover.jpg";
+import comic2Cover from "./assets/comic2Cover.jpg";
+import comic3Cover from "./assets/comic3Cover.jpg";
+
 import { Footer } from "./components/Footer";
+
+const comics: Comic[] = [
+  {
+    coverImg: comic1Cover,
+    title: "Red Goblin #9",
+    writer: "Alex Paknadel",
+    penciler: "Christopher Campana",
+  },
+  {
+    coverImg: comic2Cover,
+    title: "X-Men (2021) #27",
+    writer: "Gerry Duggan",
+    penciler: "Phil Noto",
+  },
+  {
+    coverImg: comic3Cover,
+    title: "Strange Academy: Amazing Spider-Man (2023) #1",
+    writer: "Carlos Hernandez",
+    penciler: "Vasco Georgiev",
+  },
+];
+
+interface Comic {
+  coverImg: string;
+  title: string;
+  writer: string;
+  penciler: string;
+}
 
 export function App() {
   return (
@@ -31,15 +62,16 @@ export function App() {
         </div>
 
         <div className={styles.cardsList}>
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
-          <Card coverImg={coverImg} />
+          {comics.map((comic) => {
+            return (
+              <Card
+                coverImg={comic.coverImg}
+                title={comic.title}
+                writer={comic.writer}
+                penciler={comic.penciler}
+              />
+            );
+          })}
         </div>
       </main>
 
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,16 +2,31 @@ import styles from "./Card.module.css";
 
 interface CardProps {
   coverImg: string;
+  title: string;
+  writer: string;
+  penciler: string;
 }
 
-export function Card({ coverImg }: CardProps) {
+function getLastName(fullName: string) {
+  const fullNameAsArray = fullName.split(" ");
+  const lastName = fullNameAsArray[fullNameAsArray.length - 1];
+
+  return lastName;
+}
+
+export function Card({ coverImg, title, writer, penciler }: CardProps) {
+  const writerLastName = getLastName(writer);
+  const pencilerLastName = getLastName(penciler);
+
   return (
     <div className={styles.container}>
       <img src={coverImg} alt="Cover Imagem" />
 
-      <h2>Red Goblin #9</h2>
+      <h2>{title}</h2>
 
-      <span>Paknadel, Campana</span>
+      <span>
+        {writerLastName}, {pencilerLastName}
+      </span>
     </div>
   );
 }
